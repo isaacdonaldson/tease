2211f2e7a375b9220a5ca9876e8d757bbc785e5b
Isaac <isaac.w.donaldson@gmail.com>
2024-08-06 18:57:31 -0700

    add test for error

---
+++ src/error.test.ts
@@ -0,0 +1,28 @@
+import { TaggedError, isTaggedError } from './error';
+
+describe('TaggedError', () => {
+  it('should have a _tag property', () => {
+    class TestTaggedError extends TaggedError {
+      readonly _tag = 'test';
+    }
+
+    const error = new TestTaggedError();
+    expect(error._tag).toBe('test');
+  });
+});
+
+describe('isTaggedError', () => {
+  it('should return true for TaggedError instances', () => {
+    class TestTaggedError extends TaggedError {
+      readonly _tag = 'test';
+    }
+
+    const error = new TestTaggedError();
+    expect(isTaggedError(error)).toBe(true);
+  });
+
+  it('should return false for non-TaggedError instances', () => {
+    const error = new Error();
+    expect(isTaggedError(error)).toBe(false);
+  });
+});
