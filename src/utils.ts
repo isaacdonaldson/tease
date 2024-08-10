de3a175c3501df044f73668d0e7c9d280f8ca468
Isaac <isaac.w.donaldson@gmail.com>
2024-08-10 13:52:50 -0700

    add util functions

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export function isNullable<T>(value: T | null | undefined): value is null | undefined {
+  return value === null || value === undefined;
+}
+
+export function isNonNullable<T>(value: T): value is T {
+  return !isNullable(value)
+}
