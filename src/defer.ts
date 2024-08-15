8d0daa786c8cd0a0fe14f5558a99893e569cb39d
Isaac <isaac.w.donaldson@gmail.com>
2024-08-14 19:19:22 -0700

    remove forgotten comments

--- src/defer.ts
+++ src/defer.ts
@@ -28,8 +28,6 @@ export function withDefer<T, E>(
     errdeferredCallbacks.unshift(errdeferCallback);
   };
 
-  // TODO: Make the order of the defer callbacks proper (LIFO)
-  // TODO: Write test cases for the proper order of defer callbacks
   // TODO: Make a version for async functions? (try writing async functions)
   try {
     return Result.ok(fn(defer, errdefer));
@@ -40,21 +38,3 @@ export function withDefer<T, E>(
     deferredCallbacks.forEach(callback => callback());
   }
 }
-
-// // Usage in a function
-// function example() {
-//   return withDefer((defer, errdefer) => {
-//     console.log("Start of function");
-//
-//     defer(() => {
-//       console.log("This will run at the end");
-//     });
-//
-//     console.log("Middle of function");
-//
-//     // Some code that might throw an error
-//     // ...
-//
-//     return "Function result";
-//   });
-// }
