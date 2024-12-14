86712e2e0d0343253c1acea46f3f386dc5197aa1
Isaac <isaac.w.donaldson@gmail.com>
2024-12-14 10:04:10 -0800

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/tests/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
 };
